0f4e13826f3fb9caa06fe3757dfeca818f9efc05
avAs zAde <abas.zade@outlook.com>
2026-08-10 01:09:38 +0330

    add apps to install apps

--- core/config/settings.py
+++ core/config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cart",
+    "catalog",
+    "orders",
+    "payments",
+    "users",
 ]
 
 MIDDLEWARE = [
